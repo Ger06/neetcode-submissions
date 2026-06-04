62e91f9ade77b94e5baae2b625cd838b0af727a1
Gerardo Kohatus <82271110+Ger06@users.noreply.github.com>
2026-06-04 16:46:49 -0300

    Add: eating-bananas - submission-12

---
+++ Data Structures & Algorithms/eating-bananas/submission-12.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let i = Math.ceil(h/piles.length)
+
+        let max = Math.max(...piles)
+        let candidate = h<max ? Math.floor(h/max) : Math.floor(max/h)
+        let r = 0
+        let total = 0
+        
+        while(candidate <= 1000000000){
+            total += Math.ceil(piles[r]/candidate)
+            r ++
+            if(r === piles.length){
+              if(total <= h){
+                return candidate
+                
+              }else{
+                candidate ++
+                total = 0
+                r = 0
+                }
+            }
+        }
+        return candidate
+    }
+}
